646736e1104e5e9aa46e8ba615dc6597e3827449
darius.igna@telenav.com <dariusi@DariusIgna-mbp.local>
2018-01-11 16:42:35 +0200

    Added classical threads graph coloring

---
+++ src/com/company/Graph.java
@@ -0,0 +1,145 @@
+package com.company;
+
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+/**
+ * Created by dariusi on 1/11/18.
+ */
+public class Graph {
+
+    private int E;
+    private int V;
+
+    private Set<Integer> independentSet;
+
+    private Map<Integer,NodeInfo> vInfo;
+
+    private Set<Integer> colors;
+
+    private Map<Integer, Set<Integer> > graph;
+
+    private ExecutorService exec;
+
+    public Graph(int nrOfVertices){
+        this.V = nrOfVertices;
+        graph = new HashMap<>();
+        vInfo = new HashMap<>();
+        for (int i = 0; i < nrOfVertices ; i++) {
+            graph.put(i,new HashSet<>());
+            vInfo.put(i,new NodeInfo(-1,getRandomNumberInRange(0,100)));
+        }
+        colors = new TreeSet<>();
+        independentSet = new HashSet<>(graph.keySet());
+        for (int i = 0; i < 6  ; i++) {
+            colors.add(i);
+        }
+        exec = Executors.newFixedThreadPool(8);
+    }
+
+    private static int getRandomNumberInRange(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
+    public void addEdge(int src, int dest){
+        E++;
+        graph.get(src).add(dest);
+        graph.get(dest).add(src);
+    }
+
+    public List<Integer> getNeigh(int v){
+        return graph.get(v).stream().filter((node)->{
+            return getColor(node) == -1;
+        }).collect(Collectors.toList());
+    }
+
+    public void colorGraph(){
+        while (!independentSet.isEmpty()){
+            Set<Integer> set = getIndependentSet();
+            for(Integer v : set){
+                exec.submit(()->{setColor(v);});
+            }
+            independentSet.removeAll(set);
+        }
+        exec.shutdown();
+    }
+
+    public void printColors(){
+        for(Integer v : vInfo.keySet())
+            System.out.println("V:" + v + " color:" + getColor(v) + " random:" + getValue(v));
+    }
+
+    public Integer getValue(int v){
+        return vInfo.get(v).random;
+    }
+
+    public Integer getColor(int v){
+        return vInfo.get(v).color;
+    }
+
+    private boolean checkVertext(int v){
+        for (Integer neigh: getNeigh(v)){
+            if(getValue(neigh) > getValue(v))
+                return false;
+            else if(getValue(neigh) == getValue(v) && v > neigh)
+                return false;
+        }
+        return true;
+    }
+
+    public Set<Integer> getNeighColors(int v){
+        return getAllNeighs(v).stream().filter((node) ->{
+            return getColor(node) != -1;
+        }).map ( node-> getColor(node)).distinct().collect(Collectors.toSet());
+    }
+
+    public Set<Integer> getAllNeighs(int v){
+        return graph.get(v);
+    }
+
+    public Integer getSmallestColor(int v) {
+        Set<Integer> neighColors = getNeighColors(v);
+        for (Integer c: colors){
+            if(!neighColors.contains(c))
+                return c;
+        }
+        return 0;
+    }
+
+    public void setColor(int v){
+        vInfo.get(v).color = getSmallestColor(v);
+    }
+
+    public Set<Integer> getIndependentSet(){
+        ArrayList<Future<Boolean>> list = new ArrayList<>();
+        Set<Integer> res = new HashSet<>();
+        List<Integer> l = new ArrayList<>();
+        l.addAll(independentSet);
+        for (Integer v: l){
+            Future<Boolean> f = exec.submit(()->{return checkVertext(v);});
+            list.add(f);
+        }
+
+        for (int i = 0; i < independentSet.size(); i++) {
+            try {
+                if(list.get(i).get() == true)
+                    res.add(l.get(i));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        return res;
+    }
+}
---
+++ src/com/company/NodeInfo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by dariusi on 1/11/18.
+ */
+public class NodeInfo {
+    public int color;
+    public int random;
+
+    public NodeInfo(int color, int random){
+        this.color = color;
+        this.random = random;
+    }
+}
